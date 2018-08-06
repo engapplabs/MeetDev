d90c0e4ebe89f368203092c9118b3a1f8d9d660a
ABuarque <abuarquemf@gmail.com>
2018-08-06 01:04:54 -0300

    refactored for singleton

--- lib/src/services/user_searcher.dart
+++ lib/src/services/user_searcher.dart
@@ -3,9 +3,19 @@ import "package:http/http.dart";
 
 class UserSearcherService {
 
-  static const String _API_ENDPOINT = "http://api.github.com/users/";
+  final String _API_ENDPOINT = "http://api.github.com/users/";
 
-  static getUserInfo(String userName) async {
+  static final UserSearcherService _INSTANCE = new UserSearcherService._internal();
+
+  factory UserSearcherService() {
+    return _INSTANCE;
+  }
+
+  UserSearcherService._internal() {
+
+  }
+
+  getUserInfo(String userName) async {
     var response = await get(_API_ENDPOINT + userName);
     return json.decode(response.body);
   }
