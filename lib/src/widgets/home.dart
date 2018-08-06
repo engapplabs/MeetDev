fe3cce761d1b087e3c7b4bdf2bb7e136cfc45ce1
ABuarque <abuarquemf@gmail.com>
2018-08-06 01:09:28 -0300

    home done

--- lib/src/widgets/home.dart
+++ lib/src/widgets/home.dart
@@ -1,4 +1,9 @@
 import "package:flutter/material.dart";
+import "user_info.dart";
+import "../services/user_searcher.dart";
+import "../models/user.dart";
+import "package:http/http.dart";
+import "dart:convert";
 
 class Home extends StatefulWidget {
 
@@ -9,18 +14,62 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home> {
 
+  var formKey = new GlobalKey<FormState>();
+
   String _userName;
 
-  Widget build(context) {
+  Widget getUserForm() {
+     return new TextFormField(
+       onSaved: (value) {
+         _userName = value;
+       },
+       validator: (value) {
+         if(value.length < 2) {
+           return "User name too short";
+         }
+       },
+       keyboardType: TextInputType.text,
+       decoration: new InputDecoration(
+         labelText: "User name",
+         hintText: "GitUser"
+         ),
+      );
+  }
 
-    final user = new TextFormField(
-      keyboardType: TextInputType.text,
-      initialValue: "github_user",
-
-
+  Widget getSearchButton() {
+    return new RaisedButton(
+      onPressed: () async {
+        if(formKey.currentState.validate()) {
+          formKey.currentState.save();
+          var givenInfoUser = await UserSearcherService().getUserInfo(_userName);
+          final user = new User.fromJSON(givenInfoUser);
+          Navigator.push(context, MaterialPageRoute(builder: (context) => new UserInfo(user)));
+        }
+      },
+      child: new Text("Search"),
     );
-    final searchButton = new Padding();
+  }
 
-    return null;
+  Widget build(context) {
+    return new Scaffold(
+      body: new Container(
+        margin: new EdgeInsets.all(10.0),
+        child: new Form(
+          key: formKey,
+          child: new Column(
+            children: <Widget>[
+              new Padding(
+                padding: new EdgeInsets.all(20.0),
+                child: getUserForm(),
+              ),
+              new Padding(
+                padding: new EdgeInsets.all(20.0),
+                child: getSearchButton(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
