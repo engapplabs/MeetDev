30b0263329313dd6e27e0991f29d788683d9b63f
ABuarque <abuarquemf@gmail.com>
2018-08-06 01:27:00 -0300

    button to back

--- lib/src/widgets/account_state.dart
+++ lib/src/widgets/account_state.dart
@@ -8,21 +8,45 @@ class AccountState extends StatelessWidget {
   AccountState(this.user);
 
   Widget build(context) {
-    return new Column(
-      children: <Widget>[
-        new Padding(
-          padding: new EdgeInsets.only(bottom: 10.0),
-          child: new Image.network(user.avatarUrl),
-        ),
-        new Padding(
-          padding: new EdgeInsets.only(bottom: 10.0),
-          child: new Text(user.name),
-        ),
-        new Padding(
-          padding: new EdgeInsets.only(bottom: 10.0),
-          child: new Text(user.login),
-        ),
-      ],
+    return new Scaffold(
+      body: new Column(
+        children: <Widget>[
+          new Padding(
+            padding: new EdgeInsets.only(bottom: 10.0),
+            child: new Image.network(user.avatarUrl),
+          ),
+          new Padding(
+            padding: new EdgeInsets.only(bottom: 10.0),
+            child: new Text(user.name),
+          ),
+          new Padding(
+            padding: new EdgeInsets.only(bottom: 10.0),
+            child: new Text(user.login),
+          ),
+          new Padding(
+            padding: new EdgeInsets.only(bottom: 10.0),
+            child: new Text(user.bio),
+          ),
+          new Padding(
+            padding: new EdgeInsets.only(bottom: 10.0),
+            child: new Text(user.company),
+          ),
+          new Padding(
+            padding: new EdgeInsets.only(bottom: 10.0),
+            child: new Text(user.location),
+          ),
+          new Padding(
+            padding: new EdgeInsets.only(bottom: 10.0),
+            child: new Text(user.blog),
+          ),
+        ],
+      ),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+      ),
     );
   }
 }
