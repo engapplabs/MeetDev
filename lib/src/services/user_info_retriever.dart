e05bf957f605b4dafb6f51fd8a1120fc95ecd4f5
ABuarque <abuarquemf@gmail.com>
2018-08-06 10:31:57 -0300

    service class to retrieve user info

---
+++ lib/src/services/user_info_retriever.dart
@@ -0,0 +1,27 @@
+import "../models/user.dart";
+import "../models/organization.dart";
+import "./user_searcher.dart";
+
+class UserInfoRetriever {
+
+  static UserInfoRetriever _INSTANCE = new UserInfoRetriever._internal();
+
+  factory UserInfoRetriever() {
+    return _INSTANCE;
+  }
+
+  UserInfoRetriever._internal() {
+
+  }
+
+  retrieveUserInfo(String userName) async {
+    var givenInfoUser = await UserSearcherService().getUserInfo(userName);
+    final user = new User.fromJSON(givenInfoUser);
+    var organizationsRequest = await UserSearcherService().getOrganizationsInfo(userName);
+    List<Organization> organizations = []; 
+    for (var organizationJson in organizationsRequest)
+      organizations.add(new Organization.fromJson(organizationJson));
+    user.setOrganizations(organizations);
+    return user;
+  }
+}
